714698b9f06e06f9e0699fa353afc7c942df7d42
Manna Pavan Tej <mannapavantej@gmail.com>
2026-07-22 10:33:32 +0530

    Time: 3 ms (46.06%), Space: 23.5 MB (44.34%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,6 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
-        return s.reverse()
+        p1=0
+        p2=len(s)-1
+        while p1<p2:
+            s[p1],s[p2]=s[p2],s[p1]
+            p1+=1
+            p2-=1
+        return s
         
