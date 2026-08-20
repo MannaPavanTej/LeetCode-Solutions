f4d607c22d9ad656df2dc7a4eb38cb1d4c4f53e3
Manna Pavan Tej <mannapavantej@gmail.com>
2026-08-20 17:30:55 +0530

    Time: 0 ms (100%), Space: 19.2 MB (56.32%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,8 @@
+class Solution:
+    def plusOne(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]<9:
+                nums[i]+=1
+                return nums
+            nums[i]=0
+        return [1] + nums
